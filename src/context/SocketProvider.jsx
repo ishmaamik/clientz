d218c809d86475b6b628bc3a93761cf22bc17800
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-19 03:25:41 +0600

    Update SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:5000"), []);
+  const socket = useMemo(() => io("https://socket-1-pmz3.onrender.com/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
