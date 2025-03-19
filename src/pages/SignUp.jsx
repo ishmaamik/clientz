06fc00a89d193df3992cf0aba4c2d7d3b1b16689
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-19 06:02:54 +0600

    changed

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -17,7 +17,7 @@ export default function SignUp() {
     try {
       setLoading(true);
       setError(false);
-      const res = await fetch("/api/auth/signup", {
+      const res = await fetch("https://serverz-78ek.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
